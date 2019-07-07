2ff401fd5f9ae1e3d9ee11aeb2064dcb29fece01
Padelis Theodosiou <33872406+ThPadelis@users.noreply.github.com>
2019-07-08 00:00:52 +0300

    Bootstrap app

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule {}
