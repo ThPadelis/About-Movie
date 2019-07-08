da30b620dc1bee243f0ddb10d2fc400b8b2b560e
Padelis Theodosiou <33872406+ThPadelis@users.noreply.github.com>
2019-07-08 17:55:46 +0300

    Update upcoming component

--- src/app/components/shared/upcoming/upcoming.module.ts
+++ src/app/components/shared/upcoming/upcoming.module.ts
@@ -6,10 +6,19 @@ import {
   CardsFreeModule,
   IconsModule
 } from "angular-bootstrap-md";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [UpcomingComponent],
-  imports: [ButtonsModule, WavesModule, CardsFreeModule, IconsModule],
+  imports: [
+    ButtonsModule,
+    WavesModule,
+    CardsFreeModule,
+    IconsModule,
+    CommonModule,
+    FormsModule
+  ],
   exports: [UpcomingComponent]
 })
 export class UpcomingModule {}
